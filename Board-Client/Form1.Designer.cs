a9cc59a8ec2b8aaa9ecc4293dc7c16f22dc42238
Reisi <m.reisi@ec.iut.ac.ir>
2022-08-30 00:07:23 +0430

    make a preview of the score board

--- Board-Client/Form1.Designer.cs
+++ Board-Client/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,21 +43,43 @@
             this.dataGridView1.Size = new System.Drawing.Size(240, 775);
             this.dataGridView1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(311, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(924, 799);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private Button button1;
+        private TextBox textBox1;
     }
 }
--- Board-Client/Form1.cs
+++ Board-Client/Form1.cs
@@ -1,16 +1,70 @@
+using System.Threading;
+using System.Net;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Diagnostics;
+
 namespace Board_Client
 {
     public partial class Form1 : Form
     {
+        BindingList<Team> teams;
+        List<Team> temp;
+        private static string url;
+
         public Form1()
         {
-            InitializeComponent();
-            while (true)
-            {
 
-            }
+            InitializeComponent();
+            temp = new List<Team>();
+            teams = new BindingList<Team>(temp);
+            dataGridView1.DataSource = teams;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            url = "http://" + textBox1.Text + "/board/score";
+            updateAsync();
+
         }
 
 
+        private async Task updateAsync()
+        {
+            var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+            while (await periodicTimer.WaitForNextTickAsync())
+            {
+                var wb = new WebClient();
+                var resp = wb.DownloadString(url);
+                var resp2 = JsonConvert.DeserializeObject<Dictionary<string, int>>(resp);
+                teams.Clear();
+                foreach (var x in resp2.Keys)
+                {
+                    teams.Add(new Team(x, resp2[x]));
+                }
+                temp.Sort((Team X, Team Y) => (-X.Score).CompareTo(-Y.Score));
+                teams.ResetBindings();
+                int i = 1;
+                teams[0].Rate = i;
+                for (int j = 1; j < teams.Count; j++)
+                {
+                    if (teams[j].Score < teams[j - 1].Score) i++;
+                    teams[j].Rate = i;
+                }
+            }
+
+        }
+    }
+    class Team
+    {
+        public Team(string Name, int Score)
+        {
+            this.Name = Name;
+            this.Score = Score;
+        }
+        public int Rate { set; get; }
+        public string Name { get; set; }
+        public int Score { get; set; }
     }
 }
